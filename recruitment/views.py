904206a9eef22242852593ba6c5016309e8d12c0
Rishab Sharma <np03a190051@heraldcollege.edu.np>
2023-05-19 20:12:41 +0545

    First commit

---
+++ recruitment/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import *
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def registration(request):
+    error = ""
+    if request.method == 'POST':
+        fn = request.POST['firstname']
+        ln = request.POST['lastname']
+        ec = request.POST['empcode']
+        em = request.POST['email']
+        pwd = request.POST['pwd']
+        try:
+            User.object.create_user(first_name=fn, last_name=ln, username=em, password=pwd)
+            EmployeeDetail.objects.create(empcode=ec)
+
+    return render(request, 'registration.html')
