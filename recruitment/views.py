716a8d4e523051fd00ed0e2a951234bbc3dcc84c
Rishab Sharma <np03a190051@heraldcollege.edu.np>
2023-05-20 13:15:25 +0545

    profile data received

--- recruitment/views.py
+++ recruitment/views.py
@@ -38,3 +38,23 @@ def emp_login(request):
 
 def emp_home(request):
     return render(request, 'emp_home.html')
+
+
+
+def profile(request):
+    error = ""
+    user = request.user
+    employee = EmployeeDetail.objects.get(user=user)
+    if request.method == 'POST':
+        fn = request.POST['firstname']
+        ln = request.POST['lastname']
+        ec = request.POST['empcode']
+        em = request.POST['email']
+        pwd = request.POST['pwd']
+        try:
+            user = User.objects.create_user(first_name=fn, last_name=ln, username=em, password=pwd)
+            EmployeeDetail.objects.create(user = user,empcode=ec)
+            error="no"
+        except:
+            error="yes"    
+    return render(request, 'profile.html', locals())
